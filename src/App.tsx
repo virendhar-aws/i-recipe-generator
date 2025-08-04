f7f33c458197572c150a7d3b16e3d6a5bb8d8208
Virendhar Sivaraman <virendha@amazon.com>
2025-08-04 11:58:57 +0000

    imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+import type { FormEvent } from "react";
+import { Loader, Placeholder } from "@aws-amplify/ui-react";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+
 import { FormEvent, useState } from "react";
 import { Loader, Placeholder } from "@aws-amplify/ui-react";
 import "./App.css";
